98b637c5552e3eb23df4e97c70b620e76c90d729
Nacho <ignafercosio@gmail.com>
2024-07-29 20:57:14 -0300

    Agregue el componente cards para reutilizarla en varias cartas. Estilo de Servicios terminada.

---
+++ src/Components/Cards/Cards.jsx
@@ -0,0 +1,66 @@
+import { Card, CardBody, CardFooter, Text, Image, Flex } from "@chakra-ui/react";
+import fondoVioleta from '/src/assets/images/fondoVioleta.png';
+import fondoNaranja from '/src/assets/images/fondoNaranja.png';
+
+function CustomButton({ imagen1, imagen2, imagen1Styles , imagen2Styles, titulo, texto }) {
+  return (
+    <Card 
+    w={'31.5%'} 
+    h={'420px'} 
+    bg={'#f4f4f4'} 
+    borderRadius={'30px'} 
+    textAlign={'center'} 
+    position="relative"
+    transition={'0.7s'} 
+    _hover={{ '& .imagen_principal': { opacity: 0 }, '& .fondo_principal': { opacity: 0 }, backgroundColor:'brand.morado', color:'white' }}
+    >
+      <Flex direction={'column'} justifyContent={'center'} alignItems={'center'}>
+        
+          <Image
+            src={fondoVioleta}
+            w={'130px'}
+            h={'180px'}
+            pt={'12%'}
+            position={'absolute'}
+            transform="translate(0px, -45%)"
+            opacity={'1'}
+          />
+          <Image
+            src={fondoNaranja}
+            w={'130px'}
+            h={'180px'}
+            pt={'12%'}
+            position={'absolute'}
+            transform="translate(0px, -45%)"
+            className="fondo_principal"
+            transition="opacity 0.7s ease"
+          />
+          <Image
+            src={imagen1}
+            position={'absolute'}
+            opacity={'1'}
+            mt={'70px'}
+            transform="translate(0px, -95%)"
+            {...imagen1Styles}
+
+          />
+          <Image
+            src={imagen2}
+            position={'absolute'}
+            mt={'70px'}
+            transform="translate(0px, -95%)"
+            className="imagen_principal"
+            transition="opacity 0.7s ease"
+            {...imagen2Styles}
+          />
+          <Text mt={'200px'} fontWeight={'bold'} fontSize={'22px'}>{titulo}</Text>
+
+        <CardFooter>
+          <Text lineHeight={'25px'}>{texto}</Text>
+        </CardFooter>
+      </Flex>
+    </Card>
+  );
+}
+
+export default CustomButton;
--- src/Components/Servicios/Servicios.jsx
+++ src/Components/Servicios/Servicios.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import './Servicios.css'
+import { Box, Heading, Text, Img, Flex, Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
+import datoBlanco from '/src/assets/images/datoBlanco.svg'
+import dato from '/src/assets/images/dato.svg'
+import mundoBlanco from '/src/assets/images/mundoBlanco.svg'
+import mundo from '/src/assets/images/mundo.svg'
+import dataBaseBlanco from '/src/assets/images/dataBaseBlanco.svg'
+import dataBase from '/src/assets/images/dataBase.svg'
+import Carta from '../Cards/Cards'
 
 function Servicios() {
 
   return (
-    <>
-     <p>Servicios</p>
-    </>
+    <Box as={'section'} m={'7% 0'}>
+      <Flex direction={'column'} alignItems={'center'} >
+      <Heading as={'h4'} fontSize={'20px'} color={'brand.naranja'} mb={'8px'}>SERVICIOS</Heading>
+      <Heading as={'h1'} fontSize={'38px'} mt={'0px'}>Servicios Profesionales! Démosles una mirada</Heading>
+      <Box pt={'40px'} w={'75%'}>
+        <Flex justifyContent={'space-around'} alignItems={'center'}>
+          <Carta imagen1={datoBlanco} imagen2={dato} titulo={"Desarrollo Back-End"} texto={"Me especializo en la programación orientada a objetos con Java y Python, con el objetivo de crear sistemas confiables."}></Carta>
+          <Carta imagen1={mundoBlanco} imagen2={mundo} imagen2Styles={{ transform: "translate(0px, -80%)"}} imagen1Styles={{ transform: "translate(0px, -80%)"}} titulo={"Desarrollo Front-End"} texto={"Me dedico a la creación de páginas web con Html y CSS, brindando diseños originales y animados, asegurando diseños responsive."}></Carta>
+          <Carta imagen1={dataBaseBlanco} imagen2={dataBase} imagen2Styles={{ transform: "translate(0px, -105%)"}} imagen1Styles={{ transform: "translate(0px, -105%)"}} titulo={"Bases de Datos"} texto={"Puedo crear bases de datos desde cero, administrarlas y realizar consultas, principalmente orientadas a SQL."}></Carta> 
+        </Flex>
+        
+      </Box>
+      </Flex>
+    </Box>
   )
 }
 
